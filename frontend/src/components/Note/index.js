a087ef79599c0fee6294184df789bcb0c51c643e
Joao-antonio-gg <joaoagg@al.insper.edu.br>
2023-05-04 10:41:15 -0300

    FrontEND

--- frontend/src/components/Note/index.js
+++ frontend/src/components/Note/index.js
@@ -1,13 +1,20 @@
 import React from "react";
 import "./index.css";
+import axios from "axios";
 
+const addNode = (nome) => {
+  axios
+  .post("http://127.0.0.1:8000/games/", {name: nome})
+  .then((response) => {
+  })
+}
 
 export default function Note(props) {
   return (
     <div>
         <h3>
             {props.nome} 
-            <button className="nome">Joguei!</button> 
+            <button className="nome" onClick={() => addNode(props.nome)}>Joguei!</button> 
         </h3>
     </div>
   );
